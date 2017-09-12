cd4238cab1a2df790fafd301aa68a400d7ce5f21
Corina Todoran <corina_todoran_ana@yahoo.com>
2017-09-12 09:57:46 +0300

    blurImage

--- Java/ImageProcessing/Image.java
+++ Java/ImageProcessing/Image.java
@@ -3,6 +3,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import static java.lang.Math.min;
+
 public class Image {
     private int height;
     private int width;
@@ -11,12 +13,15 @@ public class Image {
     private int[][] g;
     private int[][] b;
     private int[][] grayScale;
+    private int region;
+    private int[][][] blureMatrix;
 
     public Image(){}
     public Image(String path) {
         this.path = path;
         readImageFromFile();
         grayScaleImageFilter();
+        gaussianBlurImageFilter();
     }
 
     private void readImageFromFile() {
@@ -29,6 +34,7 @@ public class Image {
             g = new int[width][height];
             b = new int[width][height];
             grayScale = new int[width][height];
+            blureMatrix = new int[width][height][3];
 
             int[] aux;
 
@@ -90,6 +96,52 @@ public class Image {
                 aux[2] = grayScale[j][i];
                 img.getRaster().setPixel(j, i, aux);
             }
+        File outputfile = new File(fileName);
+        try {
+            ImageIO.write(img, "jpg", outputfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void gaussianBlurImageFilter()
+    {
+        for(int i = 0; i < height; i++)
+            for(int j = 0; j < width; j++)
+                blureMatrix[j][i] = getAveragePixel(i, j, 21);
+    }
+
+    private int[] getAveragePixel(int x, int y, int size)
+    {
+        int[] average = new int[3];
+        average[0] = 0;
+        average[1] = 0;
+        average[2] = 0;
+        int pix = 0;
+        for(int i = x - (size/2); i < x + (size/2)+1; i++)
+            if(i >= 0 &&  i < height)
+                for(int j = y - 1; j < y + 2; j++)
+                    if(j >= 0 && j < width)
+                        if(x != i || y != j)
+                        {
+                            pix++;
+                            average[0] += r[j][i];
+                            average[1] += g[j][i];
+                            average[2] += b[j][i];
+                        }
+        average[0] = average[0] / pix;
+        average[1] = average[1] / pix;
+        average[2] = average[2] / pix;
+
+        return average;
+    }
+
+    public void exportGaussianBlurImageToFile(String fileName)
+    {
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for(int i = 0; i < height; i++)
+            for(int j = 0; j < width; j++)
+                img.getRaster().setPixel(j, i, blureMatrix[j][i]);
         File outputfile = new File(fileName);
         try {
             ImageIO.write(img, "jpg", outputfile);
